73eab9e37aeaed8a082a1c324085b440cfe4f3e8
Sadid369 <sadid.jones@gmail.com>
2022-05-10 22:31:05 +0600

    10/5/2022

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -3,13 +3,12 @@ package com.codewithmosh;
 public class Main {
 
     public static void main(String[] args) {
-//    var employee = new Employee();
-//    employee.setBaseSalary(50000);
-//    employee.setHourlyRate(20);
-//    int wage = employee.calculateWages(10);
-//        System.out.println(wage);
+    var employee = new Employee(50000, 20);
 
-        var browser = new Browser();
+    int wage = employee.calculateWages(10);
+        System.out.println(wage);
+
+
 
 
 
