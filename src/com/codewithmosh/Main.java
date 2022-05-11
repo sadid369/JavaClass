1ff04b9c33c9c51071141e2020454282b2e54d45
Sadid369 <sadid.jones@gmail.com>
2022-05-12 01:08:08 +0600

    12/5/2022

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,16 +1,18 @@
 package com.codewithmosh;
 
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-    var employee = new Employee(50000, 20);
 
-    int wage = employee.calculateWages(10);
-        System.out.println(wage);
-
-
-
-
-
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
+        var calculator = new MortgageCalculator(principal, annualInterest,years);
+        var report = new MortgageReport(calculator);
+        report.printMortgage();
+        report.printPaymentSchedule();
     }
+
 }
